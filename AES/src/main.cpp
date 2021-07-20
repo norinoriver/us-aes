9a6525d2cafff6651f132f15e2b8d041df42c9c1
nori <norinorivertech@gmail.com>
2021-07-21 08:13:56 +0900

    [FIX]fix block size 256 to 128

--- AES/src/main.cpp
+++ AES/src/main.cpp
@@ -6,20 +6,20 @@ using namespace std;
 int main(){
 	// const 
 	// should be in the header file
-	const int block256_size = 256;
+	const int block128_size = 128;
 
 	// input text
 	string plain_text("text");
 
 	// block text
-	string block256;
-	block256.reserve(block256_size);
+	string block128;
+	block128.reserve(block128_size);
 
-	block256 += plain_text;
+	block128 += plain_text;
 	
 	// test padding
-	string padding_text(block256_size - plain_text.length(), 0x00);
-	block256 += padding_text;
+	string padding_text(block128_size - plain_text.length(), 0x00);
+	block128 += padding_text;
 
 	// 
 
