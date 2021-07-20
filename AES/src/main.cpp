590dd9f9e77a20bfe9a1ccab755a559ab5ea9c41
nori <norinorivertech@gmail.com>
2021-07-21 08:01:16 +0900

    [ADD]sample code of padding

--- AES/src/main.cpp
+++ AES/src/main.cpp
@@ -4,8 +4,24 @@
 using namespace std;
 
 int main(){
+	// const 
+	// should be in the header file
+	const int block256_size = 256;
+
+	// input text
+	string plain_text("text");
+
+	// block text
+	string block256;
+	block256.reserve(block256_size);
+
+	block256 += plain_text;
 	
-	cout << "Hello C++ World!!" << endl;
+	// test padding
+	string padding_text(block256_size - plain_text.length(), 0x00);
+	block256 += padding_text;
+
+	// 
 
 	return 0;
 
