3e9bf3b30ca66f495b1df7f9799ce4e66815f3eb
nori <norinorivertech@gmail.com>
2021-07-21 17:21:41 +0900

    [ADD]transelate to Decimal number to Binary number

--- AES/src/main.cpp
+++ AES/src/main.cpp
@@ -20,7 +20,30 @@ int main(){
 	// test padding
 	string padding_text(block_byte_size - plain_text.length(), 0x00);
 	block16 += padding_text;
-	cout << block16.length() << endl;
+	
+	// translate Decimal number to Binary number
+	for( int i = 0 ; i < (int)block16.length() ; i++ ){
+		int num = (int)plain_text[i];
+		string str;
+		str.reserve(8);
+		while(1){
+			if( num % 2 == 0 ){
+				str = "0" + str;
+			}else{
+				str = "1" + str;
+			}
+			if( num < 2 ){
+				break;
+			}
+			num /= 2;
+		}
+
+		while( str.length() < 8 ){
+			str = str + "0";
+		}
+		cout << str << endl;
+	}
+
 
 	return 0;
 
